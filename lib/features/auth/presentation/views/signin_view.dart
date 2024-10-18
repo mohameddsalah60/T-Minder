ce79a578a8ef96f5c6584fd15c98946d1ded3859
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-10-18 06:23:34 +0300

    create sign in screen

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:tmart_expiry_date/core/widgets/custom_app_bar.dart';
+
+import 'widgets/signin_view_body.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
   static const routeName = 'signIn';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(context, title: 'تسجيل الدخول'),
+      body: const SigninViewBody(),
+    );
   }
 }
