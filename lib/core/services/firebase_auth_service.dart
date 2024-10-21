32d09ef50e4000bb3cb8b49e536bdcc2693e6e97
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-10-21 04:01:50 +0300

    impl create user with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,1 +1,35 @@
-class FirebaseAuthService {}
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tmart_expiry_date/core/errors/custom_exception.dart';
+import 'package:tmart_expiry_date/core/errors/errors_messages.dart';
+
+class FirebaseAuthService {
+  final firebaseAuthInstance = FirebaseAuth.instance;
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var credential =
+          await firebaseAuthInstance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log(
+        "Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()} and code is ${e.code}",
+      );
+      throw CustomException(
+        message: ErrorsMessages.getFirebaseErrorMessage(e.code),
+      );
+    } catch (e) {
+      log(
+        "Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}",
+      );
+
+      throw CustomException(message: ErrorsMessages.genericErrorMessage);
+    }
+  }
+}
