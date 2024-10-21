9719ad99e5946e83305c60be391d49315dbc5162
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-10-21 04:39:19 +0300

    ADD get it

---
+++ lib/core/services/getit_service.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:tmart_expiry_date/core/services/firebase_auth_service.dart';
+import 'package:tmart_expiry_date/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:tmart_expiry_date/features/auth/domin/repos/auth_repo.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(
+    firebaseAuthService: getIt<FirebaseAuthService>(),
+  ));
+}
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tmart_expiry_date/core/services/getit_service.dart';
 import 'package:tmart_expiry_date/core/widgets/custom_app_bar.dart';
+import 'package:tmart_expiry_date/features/auth/domin/repos/auth_repo.dart';
+import 'package:tmart_expiry_date/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:tmart_expiry_date/features/auth/presentation/views/widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -7,16 +11,19 @@ class SignupView extends StatelessWidget {
   static const routeName = 'signUp';
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: buildAppBar(
-          context,
-          title: 'تسجيل حساب جديد',
-          onTap: () {
-            Navigator.of(context).pop();
-          },
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: buildAppBar(
+            context,
+            title: 'تسجيل حساب جديد',
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          body: const SignupViewBody(),
         ),
-        body: const SignupViewBody(),
       ),
     );
   }
